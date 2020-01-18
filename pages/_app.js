fa4e7838cf3cd79efa53760094c78dc0bd990396
Vlad V <summervibeswith9pm@gmail.com>
2020-01-18 15:48:13 +0200

    Add custom app example

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
+import styled from 'styled-components';
+
+const Layout = styled.div`
+  border: 1px dashed black;
+  padding: 20px;
+`
+
 const CustomApp = ({ Component, pageProps }) => (
-  <Component {...pageProps} />
+  <Layout>
+    <Component {...pageProps} />
+  </Layout>
 );
 
 export default CustomApp;
